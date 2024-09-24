8be7f70cf1e866eabfde49a9d07e5658a1fe3abd
Özgür Sağıroğlu <info@ozgursagiroglu.com>
2024-09-24 13:59:16 +0400

    fix: build errors fixed

--- example/config/strimus.ts
+++ example/config/strimus.ts
@@ -1,5 +1,6 @@
 export const StrimusConfig = {
   API_URL: 'https://straas-api.themachinarium.xyz',
+  SOCKET_URL: 'https://straas-api.themachinarium.xyz',
   clientId: 'client_2',
   partnerKey: 'FtmRXcxrFZfEixWM',
   partnerSecret: 'jzX88ZrV86eADJdX',
